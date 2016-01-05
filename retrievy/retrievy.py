08e02b3ab743684c1665de224a96977bb0d3371e
Aleph Melo <alephmelo@icloud.com>
2016-01-05 02:35:01 -0300

    Add random methods.

--- retrievy/retrievy.py
+++ retrievy/retrievy.py
@@ -1,17 +1,14 @@
 from tokens import Tokens as tk
+from random import randint
 import tweepy
 
 class Retrievy(object):
 
+    twitter_user_names = ['lemondefr', 'Le_Figaro']
 
     @staticmethod
-    def __get_user_name(username):
-        twitter_user_names = ['lemondefr', 'Le_Figaro']
-
-        if username in twitter_user_names:
-            return username
-        else:
-            return False
+    def __get_user_name(index):
+        return Retrievy.twitter_user_names[index]
 
 
     @staticmethod
@@ -22,15 +19,29 @@ class Retrievy(object):
         return api
 
 
-    def get_tweets(count):
-        username = Retrievy.__get_user_name('lemondefr')
+    def get_tweets(username, count):
         api = Retrievy.auth_handler()
         tweets=api.user_timeline(id=username, count=count)
-
         for tweet in tweets:
             print(tweet.text)
 
-        #tweet=tweets[1] #Get the very first tweepy.model.status object
-        #tweet_json=tweet._json 
-        #print(tweet_json['entities']['hashtags']) # this returns dict type
 
+    def random_tweet_pre():
+        random_index = randint(0, len(Retrievy.twitter_user_names) - 1)
+        username = Retrievy.__get_user_name(random_index)
+        api = Retrievy.auth_handler()
+        counter_limit = 200
+        tweets = api.user_timeline(id=username, count=counter_limit)
+        random_number = randint(0, counter_limit)
+        tweet = tweets[random_number]
+        return tweet.text
+
+
+    def random_tweet(username):
+        api = Retrievy.auth_handler()
+        counter_limit = 200
+        tweets = api.user_timeline(id=username, count=counter_limit)
+        random_number = randint(0, counter_limit)
+        print(random_number)
+        tweet = tweets[random_number]
+        return tweet.text
